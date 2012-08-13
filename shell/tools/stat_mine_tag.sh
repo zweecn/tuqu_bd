4ac71be39860df37e4c7548a2f7aff5bc9d1c257
Wei Zheng <zhengwei04@baidu.com>
2012-08-13 10:04:49 +0800

    modify shell/tools/stat_mine_tag.sh

--- shell/tools/stat_mine_tag.sh
+++ shell/tools/stat_mine_tag.sh
@@ -14,14 +14,38 @@ source_valid="data/mine_data/source_valid"
 source_valid_rm_useless="data/mine_data/source_valid.rm_useless"
 sus_out="data/mine_data/output"
 thumb=${input}"/output.thumb"
-out=${temp}".stat"
+out=${swap}"/mine.stat"
 
 mine_tag_list="conf/mine_pm_tag_count"
+
+#awk -F '\t' '{
+#	if ($3 == "")
+#		next;
+#	tag_line = $3; 
+#	gsub("\\ ", "_", tag_line);
+#	gsub("\\/", "$$", tag_line);
+#	gsub(",", "$$", tag_line);
+#	tag_line = tolower(tag_line); 
+#	delete tags;
+#	split(tag_line, tags, "$$");
+#	print tag_line;
+#	for (i in tags) {
+#		print tags[i];
+#		if (tags[i] in pm_mine) {
+#			tag_thumb[tags[i]]++;
+#		}
+#	}
+#
+#}' 1 
 
 awk -F '\t' '{
 	if (FILENAME == ARGV[1]) {
 		pm_mine[$1] = $2;	
 		tag_merged[$1] = 0;
+		tag_valid[$1] = 0;
+		tag_valid_rm[$1] = 0;
+		sus_out[$1] = 0;
+		tag_thumb[$1] = 0;
 	} else if (FILENAME == ARGV[2]){
 		tag_line = $26;
 		gsub("\\ ", "_", tag_line);
@@ -71,8 +95,11 @@ awk -F '\t' '{
 			}
 		}
 	} else {
-		tag_line = $3;
+		if ($3 == "")
+   			next;
+   		tag_line = $3;
 		gsub("\\ ", "_", tag_line);
+		gsub("\\/", "$$", tag_line);
 		gsub(",", "$$", tag_line);
 		tag_line = tolower(tag_line); 
 		delete tags;
@@ -88,6 +115,6 @@ awk -F '\t' '{
 		print i "\t" pm_mine[i] "\t" tag_merged[i] "\t" tag_valid[i] "\t" tag_valid_rm[i] "\t" sus_out[i] "\t" tag_thumb[i]; 
 	}
 
-}' ${mine_tag_list} ${merged} ${source_valid} ${source_valid_rm_useless} ${thumb} #> ${out}
+}' ${mine_tag_list} ${merged} ${source_valid} ${source_valid_rm_useless} ${sus_out} ${thumb} > ${out}
 
 echo "统计完成."
