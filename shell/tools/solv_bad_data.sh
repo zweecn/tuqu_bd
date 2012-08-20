b75857472a8643bba8c680a0e8fab222997487f6
Wei Zheng <zhengwei04@baidu.com>
2012-08-20 19:27:50 +0800

    add mine_recall.sh

--- shell/tools/solv_bad_data.sh
+++ shell/tools/solv_bad_data.sh
@@ -6,71 +6,96 @@ no_pass_tag_out="data/bad_tag_data/no_pass"
 tag_cnt="data/bad_tag_data/tag_cnt"
 pinggu_out="data/bad_tag_data/pinggu/"
 site="data/bad_tag_data/site"
+all_site="data/bad_tag_data/all_site"
 
 no_pass_tag="conf/mine_no_pass_tags"
 obj_black="conf/obj_black_list"
+tag_black="conf/err_tag"
 
-#awk -F '\t' '{
-#	if (FILENAME == ARGV[1]) {
-#		no_pass_tag[$1] = 1;
-#	} else if (FILENAME == ARGV[2] || FILENAME == ARGV[3]) {
-#		selected[$1] = 1;	
-#	} else {
-#		if ($1 in selected)
-#			next;
-#		split($3, tags, ",");
-#		for (i in tags) {
-#			tag = tags[i];
-#			if (tag in no_pass_tag){
-#				print;	
-#				break;
-#			}
-#		}
-#	}
-#
-#}' $no_pass_tag $selected ${obj_black} $mine_all > $no_pass_tag_out
-#
-#
-#awk -F '\t' '{
-#	split($3, tags, ",");
-#	for (i in tags) {
-#		tag = tags[i];
-#		tag_cnt[tag]++;
-#	}
-#} END {
-#	for (tag in tag_cnt) {
-#		print tag "\t" tag_cnt[tag];
-#	}
-#}' $no_pass_tag_out | sort -n -k2 -r > $tag_cnt
-#
-#/bin/rm -rf $pinggu_out/*
-#awk -F '\t' '{
-#	if (FILENAME == ARGV[1]) {
-#		no_pass_tag[$1] = 20;	
-#	} else {
-#		split($3, tags, ",");
-#		for (i in tags) {
-#			tag = tags[i];
-#			if (tag in no_pass_tag ) {
-#				print  > "'${pinggu_out}'"tag; 
-#			}
-#		}
-#	}
-#}' $no_pass_tag $no_pass_tag_out 
-#
+echo "Cal no pass ..."
+awk -F '\t' '{
+	if (FILENAME == ARGV[1]) {
+		no_pass_tag[$1] = 1;
+	} else if (FILENAME == ARGV[2] || FILENAME == ARGV[3]) {
+		selected[$1] = 1;	
+	} else {
+		if ($1 in selected)
+			next;
+		if (index($2, "duitang.com") || index($2, "mishang.com") || index($1, "pinfun.com"))
+			next;
+		split($3, tags, ",");
+		tag_str = "";
+		for (i in tags) {
+			tag = tags[i];
+			if (tag in no_pass_tag){
+				print;
+				break;
+			}
+		}
+	}
 
-#all_tag=`ls $pinggu_out`
-#for tag in $all_tag
-#do
-#	awk -F '\t' '{
-#		print $1 "\t" $2 "\t" $3 "\t" rand();
-#	}' $pinggu_out/$tag | sort -k4 -n | cut -f 1,2,3 | head -n 50 | sort -k1 > ${pinggu_out}/${tag}.rand50
-#done
+}' $no_pass_tag $selected ${obj_black} $mine_all  > $no_pass_tag_out
 
-#sort -k2 $no_pass_tag_out > ${no_pass_tag_out}.sort
 
+echo "Cal tag cnt ..."
+awk -F '\t' '{ 
+	split($3, tags, ",");
+	for (i in tags) {
+		tag = tags[i];
+		tag_cnt[tag]++;
+	}
+} END {
+	for (tag in tag_cnt) {
+		print tag "\t" tag_cnt[tag];
+	}
+}' $no_pass_tag_out | sort -n -k2 -r > $tag_cnt
+
+echo "Make pinggu ..."
+/bin/rm -rf $pinggu_out/*
+awk -F '\t' '{
+	if (FILENAME == ARGV[1]) {
+		no_pass_tag[$1] = 20;	
+	} else {
+		split($3, tags, ",");
+		for (i in tags) {
+			tag = tags[i];
+			if (tag in no_pass_tag ) {
+				print  > "'${pinggu_out}'"tag; 
+			}
+		}
+	}
+}' $no_pass_tag $no_pass_tag_out 
+
+
+echo "Rand 50..."
+all_tag=`ls $pinggu_out`
+for tag in $all_tag
+do
+	awk -F '\t' '{
+		print $1 "\t" $2 "\t" $3 "\t" rand();
+	}' $pinggu_out/$tag | sort -k4 -n | cut -f 1,2,3 | head -n 50 | sort -k1 > ${pinggu_out}/${tag}.rand50
+done
+
+sort -k2 $no_pass_tag_out > ${no_pass_tag_out}.sort
+
+echo "Cal no_pass site ..."
 perl -lne '{
 	if ($_ =~ /http.*?\/\/([^\/]+).*?http.*?\/\/([^\/]+)/) {
 		print $2;
 	}
 }' ${no_pass_tag_out} | sort | uniq > $site  
+
+echo "Cal all site ..."
+perl -lne '{
+	if ($_ =~ /(\w+\.\w+)\/.+?(\w+\.\w+)\//) {
+		print $2;
+	}
+}' ${mine_all} | awk '{
+	cnt[$1]++;
+} END {
+	for (s in cnt) {
+		print s "\t" cnt[s];
+	}
+}' | sort -k2 -n -r > $all_site
+
+echo "Finished."
